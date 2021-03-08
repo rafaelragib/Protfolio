154894cf077e902cea70b04ddfbc3bf3c17516f1
Ragib <ragib08@gmail.com>
2021-03-08 16:52:03 +0600

    Minor bug fix and file structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { VscGithub } from 'react-icons/vsc';
 import { FaLinkedin } from 'react-icons/fa';
 import { IoDocument } from "react-icons/io5";
 import Particles from 'react-particles-js';
-
+import {particle_settings} from './particle'
 
 
 const Header= () =>
@@ -31,119 +31,7 @@ const Header= () =>
                 
                 </Columns>
             </Container>
-        <Particles params={{
-  "particles": {
-    "number": {
-      "value": 100,
-      "density": {
-        "enable": true,
-        "value_area": 700
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 1,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 96,
-        "height": 30
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-}}>
-
-</Particles>
-
+    <Particles params={particle_settings}/>
     </Main>
     
 );
@@ -192,10 +80,10 @@ const Column= styled.div`
 
 const H2= styled.div`
     margin-top: .03em;
-    font-size:2em`;
+    font-size:2vw`;
 
 const H1= styled.div`
-    font-size: 5em;`;
+    font-size: 5vw;`;
     
 
         
